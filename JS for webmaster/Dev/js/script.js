e9ab8207878345055c512314194c3738b4065d47
k.vasiliy.r <k.vasiliy.r@users.noreply.github.com>
2018-08-25 17:48:06 +0300

    lesson 7. Done.

--- JS for webmaster/Dev/js/script.js
+++ JS for webmaster/Dev/js/script.js
@@ -28,5 +28,67 @@ window.addEventListener('DOMContentLoaded', function () {
                 }
             }
         }
-    })
+    });
+    //Timer
+    let deadline = '2018-08-26';
+    function getTimeRemaining(endtime) {
+          let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor( (t/1000) % 60 ),
+            minutes = Math.floor( (t/1000/60) % 60),
+            hours = Math.floor( (t/(1000*60*60)));
+
+          return {
+              'total': t,
+              'hours': hours,
+              'minutes': minutes,
+              'seconds': seconds,
+          };
+    }
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+        
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+        updateClock();
+        let timeInterval = setInterval(updateClock, 1000)
+    }
+        
+    setClock('timer', deadline);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
