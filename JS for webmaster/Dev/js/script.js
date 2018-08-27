65d400e4115ff6089dd45b67d7c5a8202caa40fe
k.vasiliy.r <k.vasiliy.r@users.noreply.github.com>
2018-08-27 17:44:45 +0300

    add //flowingPageAnimation function

--- JS for webmaster/Dev/js/script.js
+++ JS for webmaster/Dev/js/script.js
@@ -1,9 +1,7 @@
-
 window.addEventListener('DOMContentLoaded', function () {
     let infoHeader = document.getElementsByClassName('info-header')[0],
         infoHeaderTab = document.getElementsByClassName('info-header-tab'),
         infoTabContent = document.getElementsByClassName('info-tabcontent');
-    //console.log(infoTabContent);
     function hideTabContents(a) {
         for(let i = a; i < infoTabContent.length; i++) {
             infoTabContent[i].classList.remove('show');
@@ -29,42 +27,50 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
-    //Timer
-    let deadline = '2018-08-28';
-    function getTimeRemaining(endtime) {
-          let t = Date.parse(endtime) - Date.parse(new Date()),
-            seconds = Math.floor( (t/1000) % 60 ),
-            minutes = Math.floor( (t/1000/60) % 60),
-            hours = Math.floor( (t/(1000*60*60)));
-
-          return {
-              'total': t,
-              'hours': hours,
-              'minutes': minutes,
-              'seconds': seconds,
-          };
-    }
-    function setClock(id, endtime) {
-        let timer = document.getElementById(id),
-            hours = timer.querySelector('.hours'),
-            minutes = timer.querySelector('.minutes'),
-            seconds = timer.querySelector('.seconds');
-        
-        function updateClock() {
-            let t = getTimeRemaining(endtime);
-            hours.innerHTML = t.hours;
-            minutes.innerHTML = t.minutes;
-            seconds.innerHTML = t.seconds;
-
-            if(t.total <= 0) {
-                clearInterval(timeInterval);
-            }
-        }
-
-        updateClock();
-        let timeInterval = setInterval(updateClock, 1000)
-    }
-    setClock('timer', deadline);
+    // //Timer
+    // let deadline = '2018-08-20';
+    // function getTimeRemaining(endtime) {
+    //       let t = Date.parse(endtime) - Date.parse(new Date()),
+    //           seconds = Math.floor( (t/1000) % 60 ),
+    //           minutes = Math.floor( (t/1000/60) % 60),
+    //           hours = Math.floor( (t/(1000*60*60)));
+    //       if( t < 0 ) {
+    //           return {
+    //               'total': 0,
+    //               'hours': 0,
+    //               'minutes': 0,
+    //               'seconds': 0,
+    //           }
+    //       } else {
+    //           return {
+    //               'total': t,
+    //               'hours': hours,
+    //               'minutes': minutes,
+    //               'seconds': seconds,
+    //           }
+    //       }
+    // }
+    // function setClock(id, endtime) {
+    //     let timer = document.getElementById(id),
+    //         hours = timer.querySelector('.hours'),
+    //         minutes = timer.querySelector('.minutes'),
+    //         seconds = timer.querySelector('.seconds');
+    //
+    //     function updateClock() {
+    //         let t = getTimeRemaining(endtime);
+    //         hours.innerHTML = t.hours;
+    //         minutes.innerHTML = t.minutes;
+    //         seconds.innerHTML = t.seconds;
+    //
+    //         if(t.total <= 0) {
+    //             clearInterval(timeInterval);
+    //         }
+    //     }
+    //
+    //     updateClock();
+    //     let timeInterval = setInterval(updateClock, 1000)
+    // }
+    // setClock('timer', deadline);
 
     //Modal
 
@@ -82,6 +88,33 @@ window.addEventListener('DOMContentLoaded', function () {
         overlay.style.display = 'none';
         more.classList.remove('more-splash');
         document.body.style.overflow = '';
+    });
+    //flowingPageAnimation
+    function animate(draw, duration) {
+        let start = performance.now();
+        requestAnimationFrame(
+            function animate(time) {
+                let timePassed = time - start;
+                if (timePassed > duration) {
+                    timePassed = duration;
+                }
+
+                draw(timePassed);
+
+                if (timePassed < duration) {
+                    requestAnimationFrame(animate);
+                }
+            })
+    }
+    let menu = document.getElementsByTagName('nav')[0];
+    console.log(menu);
+    menu.addEventListener('click', function(event) {
+        event.preventDefault();
+        animate(function(timePassed) {
+            let target = event.target;
+            let section = document.getElementById(target.getAttribute('href').slice(1));
+            window.scrollBy(0, section.getBoundingClientRect().top / 20 - 3);
+        }, 1500)
     })
 
 
