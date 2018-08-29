f47bcf8a8fcf967788c0cd9c35b959dd3c00b541
k.vasiliy.r <k.vasiliy.r@users.noreply.github.com>
2018-08-29 22:24:02 +0300

    сделано: листает, оформление, Саше понравилось))) сделать: исчезание стрелочек, анимацию перехода, попробовать: листание клавишами

--- myProject/JS_Slider/script.js
+++ myProject/JS_Slider/script.js
@@ -1,16 +1,37 @@
 window.addEventListener('DOMContentLoaded', function () {
     let arr=[];
-    for(let i = 1; i < 20; i++)
-    arr.push("img/"+i+".jpg");
+
+    for(let i = 1; i < 20; i++) {
+        arr.push("img/" + i + ".jpg");
+    }
     let slider = document.getElementById('slider');
-    console.log(slider);
         slider.src = arr[0];
-    let nav = document.getElementsByClassName('nav-icon'),
-         navPrev = nav[0],
-         navNext = nav[1];
+    let navPrev = document.getElementsByClassName('prev')[0],
+        navNext = document.getElementsByClassName('next')[0],
 
+        firstSlide = 0,
+        arrLength = arr.length;
 
-    console.log(slider);
-    console.log(navNext);
-    console.log(navPrev);
+    function nextSlide() {
+        if( firstSlide === arr.length - 1){
+            firstSlide = 0;
+        } else {
+            firstSlide += 1;
+        }
+        slider.src = arr[firstSlide];
+    }
+    function prevSlide() {
+        if( firstSlide === 0){
+            firstSlide = arrLength - 1;
+        } else {
+            firstSlide -= 1;
+        }
+        slider.src = arr[firstSlide];
+    }
+    navNext.addEventListener('click', function () {
+        nextSlide();
+    });
+    navPrev.addEventListener('click', function () {
+        prevSlide();
+    });
 });
